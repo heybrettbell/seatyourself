5854f06189496a4ab2684b40960ed88e215d5682
maryfranks <maryfranks@gmail.com>
2017-07-28 17:32:45 -0400

    adds method to make reservation and to check if restaurant is full

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,34 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reservations
+
+  # @@dining_room = {6 => 0, "7" => 0, "8" => 0, "9" => 0, "10" => 0}
+
+  # def take_reservation(time, guests)
+  #   # byebug
+  #   total = @@dining_room[time] + guests
+  #   if total < 40
+  #     @@dining_room[time] += guests
+  #   else
+  #     puts "Full try again!"
+  #   end
+  # end
+
+  def take_reservation(time, guests)
+    # byebug
+    total = dining_room_current_guests(time) + guests
+    if total < 40
+      reservations.create(time: time, guests: guests)
+    else
+      puts "Full try again!"
+    end
+  end
+
+  def dining_room_current_guests(time)
+    reservations.where(time: time).map { |reservation| reservation.guests }.sum
+  end
+
+  def has_capacity?
+
+  end
 
 end
