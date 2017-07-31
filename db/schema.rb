1c2468a4701e7235b5914f88958bcc60cc73ffcd
maryfranks <maryfranks@gmail.com>
2017-07-31 14:06:06 -0400

    adds price range to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728184016) do
+ActiveRecord::Schema.define(version: 20170731180118) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "guests"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170728184016) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "cuisine"
+    t.string   "price_range"
   end
 
 end
