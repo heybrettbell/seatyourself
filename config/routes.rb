810c352e6faac901f422e4b0f21cde8aaa0b8988
maryfranks <maryfranks@gmail.com>
2017-07-31 15:53:46 -0400

    adds User routes and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'restaurants#index'
   resources :restaurants do
     resources :reservations
   end
+
+  resource :user
 
 end
