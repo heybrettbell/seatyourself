573c5eda03af4f8a22d9da2270ab06cd42b998e1
maryfranks <maryfranks@gmail.com>
2017-07-31 17:17:12 -0400

    adds view to edit reservation but functionality does not work

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -24,4 +24,28 @@ class ReservationsController < ApplicationController
 
   end
 
+  def edit
+    @reservation = Reservation.find(params[:reservation_id])
+    restaurant_id = params[:restaurant_id]
+    @restaurant = Restaurant.find(restaurant_id)
+  end
+
+  def update
+    @reservation = Reservation.find(params[:reservation_id])
+    restaurant_id = params[:restaurant_id]
+    @restaurant = Restaurant.find(restaurant_id)
+    @reservations = @restaurant.reservations
+
+    @reservation.time = params[:reservation][:time]
+    @reservation.guests = params[:reservation][:guests]
+    @reservation.restaurant_id = restaurant_id
+
+    if @reservation.save
+      flash[:notice] = "You have successfully updated this reservation!"
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render 'restaurants/show'
+    end
+  end
+
 end
